c662548baa7ac3da6323253411714277952938cc
SequoyahGeber <sequoyahgeber@gmail.com>
2024-02-18 21:51:47 -0800

    portfolio page started

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Page Loaded");
 });
+function getRepos() {
+  const apiUrl = "https://api.github.com/users/SequoyahGeber/repos";
+  const accessToken = "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";
+
+  fetch(apiUrl, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data from GitHub API:", error);
+    });
+}
